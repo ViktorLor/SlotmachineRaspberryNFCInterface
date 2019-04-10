402c00418b58b3b14955166338ec37c2877be939
Alex <alexander.demel@gmx.at>
2019-04-10 08:13:59 +0200

    Update Protokoll.cs

--- Vertrag/Protokoll.cs
+++ Vertrag/Protokoll.cs
@@ -9,8 +9,8 @@ namespace Prototype
 {
     public class Protokoll
     {
-        private static string ipadress = "192.168.0.95"; //192.168.0.95
-		private static int port = 8001; //8001
+        private static string ipadress = "172.17.7.254"; //192.168.0.95
+		private static int port = 8002; //8001
 
         private static string XmltoString(XmlDocument doc)
         {
